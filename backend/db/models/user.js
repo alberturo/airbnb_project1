57b26e1313fa314a0124ebfa228cfb2b1656dfa3
Albert Ramirez <alberturo@gmail.com>
2024-07-24 17:48:36 -0500

    migration, model and seed of user table

---
+++ backend/db/models/user.js
@@ -0,0 +1,49 @@
+"use strict";
+const { Model, Validator } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      hashedPassword: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [3, 256],
+          isEmail: true,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      defaultScope: {
+        attributes: {
+          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"],
+        },
+      },
+    }
+  );
+  return User;
+};
---
+++ backend/db/seeders/20240724211841-demo-user.js
@@ -0,0 +1,47 @@
+"use strict";
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await User.bulkCreate(
+      [
+        {
+          firstName: "Paula",
+          lastName: "Beth",
+          hashedPassword: bcrypt.hashSync("password"),
+          email: "paula@user.io",
+        },
+        {
+          firstName: "Emma",
+          lastName: "Joy",
+          hashedPassword: bcrypt.hashSync("password2"),
+          email: "emma@user.io",
+        },
+        {
+          firstName: "Lucas",
+          lastName: "Mark",
+          hashedPassword: bcrypt.hashSync("password3"),
+          email: "lucas@user.io",
+        },
+      ],
+      { validate: true }
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Users";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      options,
+      {
+        fistName: { [Op.in]: ["Paula", "Emma", "Lucas"] },
+      },
+      {}
+    );
+  },
+};
